e933eabc7f36d949118d129d8c80dc1b0cf3ec33
ldh <1114198200@qq.com>
2018-04-28 16:13:21 +0800

    log files is deleted

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+const http = require('http');
+const Redis  = require('ioredis');
+const socket = require('./src/socket');
 const app = require('./src/index.js');
 const config = require('./src/config');
+const redis = new Redis(config.redisPost,config.redisHost);
+const server = http.createServer();
+// 启动消息推送
+socket(server, redis);
+app.context.redis = redis;
+server.on('request',app.callback());
+
 app.listen(config.webPort,()=>{
 	console.log('Server listening at port %d',config.webPort);
 });
 
-
+module.exports = server;
