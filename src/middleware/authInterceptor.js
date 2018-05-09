5881e70d0405588624c44cfa2fdb64930466f5d2
ldh <1114198200@qq.com>
2018-05-09 17:30:21 +0800

    ---

--- src/middleware/authInterceptor.js
+++ src/middleware/authInterceptor.js
@@ -1,5 +1,6 @@
 //随机生成
 const uuidv4 = require('uuid/v4');
+var tokenRefreshTime = new Map();
 module.exports = async function(ctx, next){
 	let needAuthRoutes = [
 		'loopImgs',
@@ -21,8 +22,34 @@ module.exports = async function(ctx, next){
 		'300005',
 		'300004'
 	];
-	console.log(ctx.url);
-	await next();
+	console.log(ctx.req.url);
+	if(needAuthRoutes.indexOf(ctx.req.url.splice(1)) > -1){
+		ctx.tokenId ;
+		let expiredTime = 4320000;//3天
+		let refreshTime = tokenRefreshTime.get(ctx.tokenId);
+		if(Date.now() - refreshTime < expiredTime){
+			tokenRefreshTime.set(ctx.tokenId, Date.now())
+		}else{
+			
+		}
+		//key = 'user:tokenId'
+		//key = 'fund:tokenId'
+		var val = await ctx.redis.get('user:tokenId');
+		if(val){
+			ctx.body = {
+				error_no : '9999',
+				error_info : '登录时间过期！'
+			}
+			return;
+		}else{
+			//刷新时间
+		}
+		await next();
+	}else{
+		await next();
+	}
+	
+	
 	
 	
 }
